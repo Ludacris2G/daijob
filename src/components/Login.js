40274b6a2687a2ad4c106f2cbdca11801d9995a4
Liviu <liviutoma946@gmail.com>
2023-04-20 16:19:08 +0200

    Initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,138 @@
+import styled from 'styled-components'
+import React from 'react'
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Nav>
+        <a href="/">Daijob</a>
+        <div>
+          <Join>Join now</Join>
+          <SignIn>Sign in</SignIn>
+        </div>
+      </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community.</h1>
+          <img src='/images/company_roudou_kumiai.png' alt="Company employees greeting you!" />
+        </Hero>
+        <Google>
+
+        </Google>
+      </Section>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  padding: 0px;
+`;
+
+const Nav = styled.div`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  & > a {
+    width: 135px;
+    height: 34px;
+    text-decoration: none;
+    font-size: 2rem;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+
+const Join = styled.a`
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  border-radius: 4px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
+`;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167px;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background-color: rgba(112, 141, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: flex-start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -100px;
+    @media (max-width: 767px) {
+      top: 230px;
+      width: 100%;
+      height: initial;
+      position: initial;
+    }
+  }
+`;
+
+const Google = styled.div`
+
+`;
+
+export default Login
