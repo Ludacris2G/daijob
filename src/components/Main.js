db34674ffc749f1710d3bcf4f3e6cab20ae987e4
Liviu <liviutoma946@gmail.com>
2023-04-25 23:48:32 +0300

    added post card

--- src/components/Main.js
+++ src/components/Main.js
@@ -46,6 +46,46 @@ function Main({ props }) {
                 <img src="/images/three-dots.svg" alt="" />
               </button>
             </SharedActor>
+            <Description>
+              Description
+            </Description>
+            <SharedImg>
+              <a>
+                <img src="/images/shared-image.jpg" alt="" />
+              </a>
+            </SharedImg>
+            <SocialCounts>
+              <li>
+                <button>
+                  <img src="/images/親指.png" alt="" />
+                  <img src="/images/washing-hands.png" alt="" />
+                  <span>75</span>
+                </button>
+              </li>
+              <li>
+                <a>
+                  2 comments
+                </a>
+              </li>
+            </SocialCounts>
+            <SocialActions>
+              <button>
+                <img src="/images/親指.png" alt="" />
+                <span>Like</span>
+              </button>
+              <button>
+                <img src="/images/megaphone.png" alt="" />
+                <span>Comments</span>
+              </button>
+              <button>
+                <img src="/images/share.png" alt="" />
+                <span>Share</span>
+              </button>
+              <button>
+                <img src="/images/紙飛行機.png" alt="" />
+                <span>Send</span>
+              </button>
+            </SocialActions>
           </Article>
         </div>
       </Container>
@@ -139,6 +179,112 @@ const Article = styled(CommonCard)`
 const SharedActor = styled.div`
   padding-right: 40px;
   flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n+1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+  button {
+    display: inline-flex;
+    align-items: center;
+    padding: 8px;
+    color: #0a66c2;
+    height: 40px;
+    margin-right: 5px;
+    @media (min-width: 768px) {
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
 `;
 
 export default Main
