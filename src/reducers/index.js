2dff4bccbd28302eb1729308497dd81c1c054948
Liviu <liviutoma946@gmail.com>
2023-04-26 19:33:23 +0300

    added reducers file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer.js';
+import { combineReducers } from 'redux'
+import userReducer from './userReducer.js'
 
 const rootReducer = combineReducers({
     userState: userReducer,
