7de23f8e039869b24f7c87f8a182eaa35feb006d
Liviu <liviutoma946@gmail.com>
2023-05-02 18:24:53 +0300

    fixed article order

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app'
-import { auth, provider, signInWithPopup, storage, GoogleAuthProvider, getStorage, collection, addDoc, serverTimestamp, uploadBytesResumable, doc, getDoc, orderBy } from '../firebase'
+import { auth, provider, signInWithPopup, storage, GoogleAuthProvider, getStorage, collection, addDoc, serverTimestamp, uploadBytesResumable, doc, getDoc, orderBy, query } from '../firebase'
 import { onSnapshot } from 'firebase/firestore'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import db from '../firebase'
@@ -128,9 +128,9 @@ export function getArticlesAPI() {
 
   return (dispatch) => {
     console.warn('feature????')
-    const unsub = onSnapshot(
-      collection(db, "articles"), 
-      (querySnapshot) => {
+    const articlesRef = collection(db, 'articles');
+    const q = query(articlesRef, orderBy('actor.date', 'desc'));
+    const unsub = onSnapshot(q, orderBy('date', 'asc'), (querySnapshot) => {
       payload = querySnapshot.docs.map((doc) => doc.data());
       dispatch(getArticles(payload));
     });
